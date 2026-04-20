b83aee792bdbc03b9c04c7f54fb874f127ac124a
Karen Gozeyan <kgozeyan@griddynamics.com>
2026-04-20 23:50:00 +0400

    chore: initial commit

---
+++ src/main/java/com/griddynamics/reactive_paradigm_capstone/ReactiveParadigmCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.griddynamics.reactive_paradigm_capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveParadigmCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveParadigmCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/griddynamics/reactive_paradigm_capstone/ReactiveParadigmCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamics.reactive_paradigm_capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveParadigmCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
